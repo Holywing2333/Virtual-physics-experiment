fbb35a893b27892913c0f8f6e0a1549e913eb4f8
Holywing2333 <626291261@qq.com>
2022-02-26 12:17:03 +0800

    init

---
+++ challengeCup/Assets/c#/BorderMove.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BorderMove : MonoBehaviour
+{
+	public float cameraMoveSpeed;
+	public float camerZoomSpeed;
+	public float borderWidth = 50f;
+	public Vector2 xClamp;
+	public Vector2 yClamp;
+	public Vector2 zClamp;
+	private void Update()
+	{
+		Vector2 mousePos = Input.mousePosition;
+		//Move Right
+			if (mousePos.x > Screen.width - borderWidth)
+			{
+				transform.Translate(transform.right * cameraMoveSpeed * Time.deltaTime);
+			}
+			//Move Left
+			if (mousePos.x < borderWidth)
+			{
+				transform.Translate(-transform.right * cameraMoveSpeed * Time.deltaTime);
+			}
+			//Move Up
+			if (mousePos.y > Screen.height - borderWidth)
+			{
+				transform.Translate(-transform.forward * cameraMoveSpeed * Time.deltaTime);
+			}
+			//Move Down
+			if (mousePos.y < borderWidth)
+			{
+				transform.Translate(transform.forward * cameraMoveSpeed * Time.deltaTime);
+			}
+		float scrollWheel = Input.GetAxis("Mouse ScrollWheel");
+		transform.Translate(transform.up * scrollWheel * camerZoomSpeed * Time.deltaTime);
+		Vector3 CPos = transform.position;
+		CPos .x=Mathf .Clamp (CPos.x,xClamp.x,xClamp .y );
+		CPos .y=Mathf .Clamp (CPos.y,yClamp .x,yClamp .y );
+		CPos .z=Mathf .Clamp (CPos.z,zClamp.x,zClamp .y );
+		transform.position = CPos;
+	}
+}
---
+++ challengeCup/Assets/c#/move.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+	private Vector3 offset;
+	private Vector3 depth;
+
+	// Update is called once per frame
+	private void OnMouseDown()
+	{
+		depth=Camera.main.WorldToScreenPoint(transform.position);
+		Vector3 mousePosition=Input.mousePosition;
+		mousePosition=new Vector3(mousePosition.x, mousePosition.y,depth .z );
+		offset = Camera.main.ScreenToWorldPoint(mousePosition) - transform.position;
+		GetComponent<Rigidbody>().isKinematic = true;
+	}
+
+	private void OnMouseDrag () 
+	{
+	Vector3 mousePosition=Input.mousePosition;
+		mousePosition = new Vector3(mousePosition.x,mousePosition.y,depth .z );
+	transform.position=Camera.main.ScreenToWorldPoint(mousePosition)-offset;
+	}
+
+	private void OnMouseUp()
+	{
+		GetComponent<Rigidbody>().isKinematic = false;
+	}
+
+}
